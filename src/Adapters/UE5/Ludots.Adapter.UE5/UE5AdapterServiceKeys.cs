b65417ce5de3215f6b4f59bb8892ae71b97432f8
hzxueqi <hzxueqi@corp.netease.com>
2026-04-12 13:14:40 +0800

    Add UE5 host camera diagnostics contracts

--- src/Adapters/UE5/Ludots.Adapter.UE5/UE5AdapterServiceKeys.cs
+++ src/Adapters/UE5/Ludots.Adapter.UE5/UE5AdapterServiceKeys.cs
@@ -12,5 +12,8 @@ namespace Ludots.Adapter.UE5
         public static readonly ServiceKey<IExternalSessionTransitionHandler> ExternalSessionTransitionHandler = new("UE5.ExternalSessionTransitionHandler");
         public static readonly ServiceKey<IHostBoundMapSessionService> HostBoundMapSessionService = new("UE5.HostBoundMapSessionService");
         public static readonly ServiceKey<HostBoundMapSessionSnapshot> HostBoundMapSessionState = new("UE5.HostBoundMapSessionState");
+        public static readonly ServiceKey<UE5SharedCameraState> SharedCameraState = new("UE5.SharedCameraState");
+        public static readonly ServiceKey<UE5HostCameraDiagnosticsSnapshot> HostCameraDiagnosticsSnapshot = new("UE5.HostCameraDiagnosticsSnapshot");
+        public static readonly ServiceKey<UE5HostCameraDiagnosticsCommandState> HostCameraDiagnosticsCommands = new("UE5.HostCameraDiagnosticsCommands");
     }
 }
